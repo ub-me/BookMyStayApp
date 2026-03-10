5d2a85ba1ae9b3ecdd4191c05f9f8d74ccad9b1e
ub-me <ujjwal.connect.dev@gmail.com>
2026-03-10 22:38:35 +0530

    UC1: Application Entry and Welcome Message implemented

---
+++ src/UseCase1HotelBookingApp.java
@@ -0,0 +1,12 @@
+
+public class UseCase1HotelBookingApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("==================================");
+        System.out.println(" Welcome to Book My Stay App ");
+        System.out.println(" Hotel Booking System v1.0 ");
+        System.out.println("==================================");
+
+    }
+}
